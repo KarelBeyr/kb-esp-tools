4c33d19c3267661270dc5633145eb80a51ab9ff9
KarelBeyr <karel.beyr@gmail.com>
2019-06-30 12:12:47 +0200

    removed passwords.h

--- thingspeak/thingspeak.cpp
+++ thingspeak/thingspeak.cpp
@@ -2,14 +2,9 @@
 #include "thingspeak.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <passwords.h>
-//#include "tools.h"
-//#include "led.h"
-
 unsigned long lastConnectionTime = 0;
 
 const char *thingSpeakServer = "api.thingspeak.com";
-//const char *thingSpeakServer = "postman-echo.com/post";
 
 const unsigned long postingInterval = 15L * 1000L;
 
@@ -76,11 +71,11 @@ String iso8601time() {
 
 void logMessageToThingspeak(String message)
 {
-  String data = "field1=" + Passwords().CurrentLocationName()
-     + "&field2=" + iso8601date()
-     + "&field3=" + iso8601time()
-    + "&field4=" + message;
-  postDataToThingspeak(data, "NB4KBVX0CE6J6HUF");
+//   String data = "field1=" + Passwords().CurrentLocationName()
+//      + "&field2=" + iso8601date()
+//      + "&field3=" + iso8601time()
+//     + "&field4=" + message;
+//   postDataToThingspeak(data, "NB4KBVX0CE6J6HUF");
 }
 
 void maybeLogTelemetryToThingspeak(String apiKey, unsigned long interval, String d1, String d2, String d3, String d4, String d5, String d6, String d7, String d8)
@@ -107,44 +102,3 @@ void maybeLogTelemetryToThingspeak(String apiKey, unsigned long interval, String
 void maybeLogTelemetryToThingspeak(String apiKey, unsigned long interval, String d1, String d2, String d3, String d4, String d5) { maybeLogTelemetryToThingspeak(apiKey, interval, d1, d2, d3, d4, d5, "0", "0", "0"); }
 void maybeLogTelemetryToThingspeak(String apiKey, unsigned long interval, String d1, String d2, String d3, String d4, String d5, String d6) { maybeLogTelemetryToThingspeak(apiKey, interval, d1, d2, d3, d4, d5, d6, "0", "0"); }
 void maybeLogTelemetryToThingspeak(String apiKey, unsigned long interval, String d1, String d2, String d3, String d4, String d5, String d6, String d7) { maybeLogTelemetryToThingspeak(apiKey, interval, d1, d2, d3, d4, d5, d6, d7, "0"); }
-
-/*
-void postTelemetryToThingspeak(State *state)
-{
-  String data = "field1=" + String(state->freq) + "&field2=" + String(state->duty) +
-      "&field3=" + String(state->desiredLux) + "&field4=" + String(state->measuredLux) +
-      "&field5=" + String(state->automaticMode) + "&field6=" + String(state->utlumStin) +
-      "&field7=" + String(state->dutyFor100lux) + "&field8=" + String(state->currentDuty);
-  postDataToThingspeak(data, Passwords().ApiKey());
-  lastConnectionTime = millis();
-}
-
-
-void thingSpeakGetWindowLux(State *state)
-{
-    HTTPClient http;
-    http.begin("http://api.thingspeak.com/channels/414735/fields/1/last.txt");
-    int httpCode = http.GET();
-    float res = 0;
-    if (httpCode == 200)
-    { //Check for the returning code
-        String payload = http.getString();
-        state->measuredLux = payload.toFloat();
-    }
-    else
-    {
-        Serial.println("Error on HTTP request");
-    }
-    http.end();
-}
-
-void maybeInquireThingspeak(State *state)
-{
-    if (millis() - lastConnectionTime > postingInterval)
-    {
-        postTelemetryToThingspeak(state);
-        thingSpeakGetWindowLux(state);
-        changeLedPwm(state);
-    }
-}
-*/
