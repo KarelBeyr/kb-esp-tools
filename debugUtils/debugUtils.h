ed0684353d1fa7addcb6792273e71ff39f8c515e
KarelBeyr <karel.beyr@gmail.com>
2019-06-30 15:36:05 +0200

    debug utils - conditional printing to serial. Controlled by commenting out #define DEBUG at the beginning of this file - maybe not the best solution? But at least its only in one place

---
+++ debugUtils/debugUtils.h
@@ -0,0 +1,17 @@
+//https://forum.arduino.cc/index.php?topic=64555.0
+#define DEBUG
+
+#ifndef DEBUGUTILS_H
+#define DEBUGUTILS_H
+
+#ifdef DEBUG
+  #define DEBUG_PRINT(...) Serial.print(__VA_ARGS__)
+  #define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__)
+  #define DEBUG_SERIAL_START(...) Serial.begin(__VA_ARGS__)
+#else
+  #define DEBUG_PRINT(...)
+  #define DEBUG_PRINTLN(...)
+  #define DEBUG_SERIAL_START(...)
+#endif
+
+#endif
